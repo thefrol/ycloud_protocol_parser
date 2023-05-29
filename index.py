a721b3e26e3c6fbffb43fc86127422f93d9ecb79
Дмитрий Фроленко <orangefrol@gmail.com>
2023-05-29 16:59:58 +0300

    added player_url parsing

--- index.py
+++ index.py
@@ -1,44 +1,69 @@
-from protocol_parsers import MosffParser
+from protocol_parsers import MosffParser, MosffPlayerParser
 
-POST_URL_PARAMETER_NAME='protocol_url'
-POST_MATCH_TIME_PARAMETER_NAME='match_time'
+PROTOCOL_URL_PARAMETER_NAME='protocol_url'
+MATCH_TIME_PARAMETER_NAME='match_time'
+PLAYER_URL_PARAMETER_NAME='player_url'
+
 
 def check_input(event):
-    if event['queryStringParameters'].get(POST_URL_PARAMETER_NAME) is None:
+    if event['queryStringParameters'].get(PROTOCOL_URL_PARAMETER_NAME):
+        if event['queryStringParameters'].get(MATCH_TIME_PARAMETER_NAME) is not None:
+            try:
+                int(event['queryStringParameters'].get(MATCH_TIME_PARAMETER_NAME))
+            except Exception:
+                return False
+        return 'protocol'
+    elif event['queryStringParameters'].get(PLAYER_URL_PARAMETER_NAME):
+        return 'player'
+    else:
         return False
-    if event['queryStringParameters'].get(POST_MATCH_TIME_PARAMETER_NAME) is not None:
-        try:
-            int(event['queryStringParameters'].get(POST_MATCH_TIME_PARAMETER_NAME))
-        except Exception:
-            return False
-    return True
+    
 
 def handler(event, context):
-    if not check_input(event):
+
+    input_type=check_input(event)
+    if not input_type:
             return {
         'statusCode': 400,
-        'body': f'Bad input, maybe field {POST_URL_PARAMETER_NAME} not specified. or {POST_MATCH_TIME_PARAMETER_NAME} cant be converted to int',
+        'body': f'Bad input, maybe field {PROTOCOL_URL_PARAMETER_NAME} or {PLAYER_URL_PARAMETER_NAME} not specified. or {MATCH_TIME_PARAMETER_NAME} cant be converted to int',
     }
 
-    protocol_URL=event['queryStringParameters'].get(POST_URL_PARAMETER_NAME)
-    match_time=event['queryStringParameters'].get(POST_MATCH_TIME_PARAMETER_NAME)
+    protocol_URL=event['queryStringParameters'].get(PROTOCOL_URL_PARAMETER_NAME)
+    player_URL=event['queryStringParameters'].get(PLAYER_URL_PARAMETER_NAME)
+    match_time=event['queryStringParameters'].get(MATCH_TIME_PARAMETER_NAME)
 
-    if match_time:
-        match_time=int(match_time)
 
-    try:
-        result=MosffParser(protocol_URL,match_time).to_rbdata()
+    if input_type=='protocol':
+        try:
+            if match_time:
+                match_time=int(match_time)
+            result=MosffParser(protocol_URL,match_time).to_rbdata()
 
-        return {
-           'statusCode': 200,
-            'body': {
-                'protocol':result,
-                'log':None
-            },
-        }
-    except:
-        return {
-            'statusCode': 500,
-            'body': 'parsing failed',
-         }
+            return {
+            'statusCode': 200,
+                'body': {
+                    'protocol':result,
+                    'log':None
+                },
+            }
+        except:
+            return {
+                'statusCode': 500,
+                'body': 'parsing protocol failed',
+            }
+    if input_type=='player':
+        try:
+            result=MosffPlayerParser(player_URL).to_rbdata()
 
+            return {
+            'statusCode': 200,
+                'body': {
+                    'player':result,
+                    'log':None
+                },
+            }
+        except:
+            return {
+                'statusCode': 500,
+                'body': f'parsing player {player_URL} failed',
+            }
