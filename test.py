fff3ac9172684a62aff612cff0f4cbb9ff62b24e
Дмитрий Фроленко <orangefrol@gmail.com>
2023-05-31 20:31:03 +0300

    description bug

--- test.py
+++ test.py
@@ -3,21 +3,22 @@ import unittest
 import requests
 
 class ServiceTest(unittest.TestCase):
+    '''doc'''
     url=None
     def setUp(self) -> None:
         pass
     def test_protocol_service(self):
         '''protocol service avialability'''
-        service_url=self.url+'?protocol_url=https://mosff.ru/match/34549'
+        service_url=self.url+'?protocol_url=https://mo2sff.ru/match/34549'
         r=requests.get(service_url)
         self.assertTrue(r.ok,'something bad with protocol service')
     def test_player_service(self):
-        '''protocol service avialability'''
+        '''player service avialability'''
         service_url=self.url+'?player_url=https://mosff.ru/player/2050'
         r=requests.get(service_url)
         self.assertTrue(r.ok,'something bad with player service')
     def test_team_service(self):
-        '''protocol service avialability'''
+        '''team service avialability'''
         service_url=self.url+'?team_url=https://mosff.ru/team/2051'
         r=requests.get(service_url)
         self.assertTrue(r.ok,'something bad with team service')
